7ca20612b7a30ef25d3d2ebe85a64179d34fbee4
Zhang Rui <bbcallen@gmail.com>
2013-07-03 21:46:17 +0800

    avformat/hls: parse #EXT-X-MEDIA-SEQUENCE only once

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -682,6 +682,7 @@ static int parse_playlist(HLSContext *c, const char *url,
     struct variant_info variant_info;
     char tmp_str[MAX_URL_SIZE];
     struct segment *cur_init_section = NULL;
+    int start_seq_no = -1;
 
     if (!in) {
 #if 1
@@ -758,7 +759,11 @@ static int parse_playlist(HLSContext *c, const char *url,
             ret = ensure_playlist(c, &pls, url);
             if (ret < 0)
                 goto fail;
-            pls->start_seq_no = atoi(ptr);
+            /* Some buggy HLS servers write #EXT-X-MEDIA-SEQUENCE more than once */
+            if (start_seq_no < 0) {
+                start_seq_no = atoi(ptr);
+                pls->start_seq_no = start_seq_no;
+            }
         } else if (av_strstart(line, "#EXT-X-PLAYLIST-TYPE:", &ptr)) {
             ret = ensure_playlist(c, &pls, url);
             if (ret < 0)
