8390dbec3ef858c131a66f0f8c1d60016f23098f
Zhang Rui <bbcallen@gmail.com>
2013-07-17 16:45:52 +0800

    avformat/aviobuf: enlarge IO_BUFFER_SIZE and SHORT_SEEK_THRESHOLD

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -33,14 +33,14 @@
 #include "url.h"
 #include <stdarg.h>
 
-#define IO_BUFFER_SIZE 32768
+#define IO_BUFFER_SIZE (32768 * 2)
 
 /**
  * Do seeks within this distance ahead of the current buffer by skipping
  * data instead of calling the protocol seek function, for seekable
  * protocols.
  */
-#define SHORT_SEEK_THRESHOLD 4096
+#define SHORT_SEEK_THRESHOLD (4096 * 8)
 
 static void *ffio_url_child_next(void *obj, void *prev)
 {
