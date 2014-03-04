50bb8f86d311992fa25adc75e014db64b06234bf
bbcallen <bbcallen@gmail.com>
2014-03-04 14:15:58 +0800

    avformat/concatdec: close previous segment only after new segment is open successfully
    Later call to concat_read_packet could cause NULL pointer access.

--- libavformat/concatdec.c
+++ libavformat/concatdec.c
@@ -62,6 +62,7 @@ typedef struct {
     int eof;
     ConcatMatchMode stream_match_mode;
     unsigned auto_convert;
+    int error;
 } ConcatContext;
 
 static int concat_probe(AVProbeData *probe)
@@ -289,26 +290,37 @@ static int open_file(AVFormatContext *avf, unsigned fileno)
 {
     ConcatContext *cat = avf->priv_data;
     ConcatFile *file = &cat->files[fileno];
+    AVFormatContext *new_avf = NULL;
     int ret;
+
+    new_avf = avformat_alloc_context();
+    if (!new_avf)
+        return AVERROR(ENOMEM);
+
+    new_avf->interrupt_callback = avf->interrupt_callback;
+
+    if ((ret = ff_copy_whitelists(new_avf, avf)) < 0)
+        return ret;
+
+    if (cat->options)
+        av_dict_copy(&tmp, cat->options, 0);
+
+    ret = avformat_open_input(&new_avf, file->url, NULL, &tmp);
+    av_dict_free(&tmp);
+    if (ret < 0 ||
+        (ret = avformat_find_stream_info(new_avf, NULL)) < 0) {
+        av_log(avf, AV_LOG_ERROR, "Impossible to open '%s'\n", file->url);
+        avformat_close_input(&new_avf);
+        return ret;
+    }
+
+    if (!new_avf)
+        return 0;
 
     if (cat->avf)
         avformat_close_input(&cat->avf);
 
-    cat->avf = avformat_alloc_context();
-    if (!cat->avf)
-        return AVERROR(ENOMEM);
-
-    cat->avf->interrupt_callback = avf->interrupt_callback;
-
-    if ((ret = ff_copy_whitelists(cat->avf, avf)) < 0)
-        return ret;
-
-    if ((ret = avformat_open_input(&cat->avf, file->url, NULL, NULL)) < 0 ||
-        (ret = avformat_find_stream_info(cat->avf, NULL)) < 0) {
-        av_log(avf, AV_LOG_ERROR, "Impossible to open '%s'\n", file->url);
-        avformat_close_input(&cat->avf);
-        return ret;
-    }
+    cat->avf      = new_avf;
     cat->cur_file = file;
     if (file->start_time == AV_NOPTS_VALUE)
         file->start_time = !fileno ? 0 :
@@ -534,6 +546,7 @@ static int packet_after_outpoint(ConcatContext *cat, AVPacket *pkt)
     return 0;
 }
 
+#define CONCAT_MAX_OPEN_TRY 3
 static int concat_read_packet(AVFormatContext *avf, AVPacket *pkt)
 {
     ConcatContext *cat = avf->priv_data;
@@ -541,6 +554,12 @@ static int concat_read_packet(AVFormatContext *avf, AVPacket *pkt)
     int64_t delta;
     ConcatStream *cs;
     AVStream *st;
+    int try_counter = 0;
+
+    if (cat->error) {
+        ret = cat->error;
+        return ret;
+    }
 
     if (cat->eof)
         return AVERROR_EOF;
@@ -553,8 +572,17 @@ static int concat_read_packet(AVFormatContext *avf, AVPacket *pkt)
         if (ret == AVERROR_EOF || packet_after_outpoint(cat, pkt)) {
             if (ret == 0)
                 av_packet_unref(pkt);
-            if ((ret = open_next_file(avf)) < 0)
-                return ret;
+            if ((ret = open_next_file(avf)) < 0) {
+                ++try_counter;
+                if (try_counter > CONCAT_MAX_OPEN_TRY) {
+                    cat->error = ret;
+                    if (avf->pb && ret != AVERROR_EOF)
+                        avf->pb->error = ret;
+                    return AVERROR_EOF;
+                }
+
+                av_log(avf, AV_LOG_WARNING, "open_next_file() failed (%d)\n", try_counter);
+            }
             continue;
         }
         if (ret < 0)
@@ -678,6 +706,9 @@ static int concat_seek(AVFormatContext *avf, int stream,
     ConcatFile *cur_file_saved = cat->cur_file;
     AVFormatContext *cur_avf_saved = cat->avf;
     int ret;
+
+    /* reset error/complete state */
+    cat->error = 0;
 
     if (!cat->seekable)
         return AVERROR(ESPIPE); /* XXX: can we use it? */
